7634d09d95e7362ab3e1d9029c673e89bcf9b22a
carlygaejepsen <ttiresearch.dani@gmail.com>
2025-02-01 21:48:31 -0500

    Update script.js

--- static/script.js
+++ static/script.js
@@ -44,6 +44,7 @@ async function loadData() {
 }
         
         // Assign data to global variables
+        characterCards = cardData.cards.filter(card => card.type === "character");
         actionCards = cardData.classAttacks;
         elementCards = cardData.elementAttacks;
         classMatchingRules = battleData.classMatchingRules;
