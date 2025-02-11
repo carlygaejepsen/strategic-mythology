4ba53454c1132c806d93c8206f8b6d71b4deff12
carlygaejepsen <ttiresearch.dani@gmail.com>
2025-02-11 15:15:29 -0500

    Update battle.js

--- battle.js
+++ battle.js
@@ -1,7 +1,7 @@
 import { processCombat, battleSystem } from "./battle-logic.js";
 import { drawCardsToFillHands, setSelectedAttacker, setSelectedDefender, setPlayerHasPlacedCard, setEnemyHasPlacedCard } from "./interact.js";
 import { logToResults, getRandomCardFromZone } from "./display.js";
-import { currentPlayerBattleCards, currentEnemyBattleCards, playerHasPlacedCard, enemyHasPlacedCard, } from "./config.js";
+import { currentPlayerBattleCards, currentEnemyBattleCards, playerHasPlacedCard, enemyHasPlacedCard, playerDeck, enemyDeck } from "./config.js";
 let gameRunning = false;
 
 //gameLoop 3.0
