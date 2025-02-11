7510b9f6c099d96acc5bb1863ca8b8fffa805770
carlygaejepsen <ttiresearch.dani@gmail.com>
2025-02-11 14:04:59 -0500

    Update display.js

--- display.js
+++ display.js
@@ -1,6 +1,6 @@
 import { createCardElement } from "./cards.js";
 import { determineCardType } from "./cards.js";
-import { playerHand, enemyHand }
+import { playerHand, enemyHand } from "./config.js";
 import { setEnemyHasPlacedCard, placeCardInBattleZone } from "./interact.js";
 
 export function logToResults(message) {
